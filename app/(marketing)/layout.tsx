f922b734f2e1a44a0fab3f29e3afb16bbc4987b9
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-21 12:44:46 +1300

    rootLayout

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -3,7 +3,9 @@ import React from 'react'
 const MarketingLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-full bg-slate-100">
+      {/* Nav bar */}
       <main className="pt-40 pb-20 bg-slate-100">{children}</main>
+      {/* Footer */}
     </div>
   )
 }

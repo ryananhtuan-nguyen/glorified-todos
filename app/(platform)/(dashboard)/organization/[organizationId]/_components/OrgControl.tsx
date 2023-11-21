55c3c4b9ab95a16c358df874ab5fb029b16f81a5
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-21 18:48:46 +1300

    org control

--- app/(platform)/(dashboard)/organization/[organizationId]/_components/OrgControl.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/OrgControl.tsx
@@ -1,10 +1,20 @@
 'use client'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useOrganizationList } from '@clerk/nextjs'
 import { useParams } from 'next/navigation'
 
 const OrgControl = () => {
   const params = useParams()
+  const { setActive } = useOrganizationList()
+
+  useEffect(() => {
+    if (!setActive) return
+
+    setActive({
+      organization: params.organizationId as string,
+    })
+  }, [setActive, params.organizationId])
+
   return null
 }
 

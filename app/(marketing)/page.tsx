002d903cda6974fee7e0cc7b719f76677eb54bc3
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-21 13:35:56 +1300

    main part done

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,12 +1,29 @@
+import Link from 'next/link'
 import { Medal } from 'lucide-react'
+import localFont from 'next/font/local'
+import { Poppins } from 'next/font/google'
 
 import { Button } from '@/components/ui/button'
-import Link from 'next/link'
+import { cn } from '@/lib/utils'
+
+const headingFont = localFont({
+  src: '../../public/fonts/font.woff2',
+})
+
+const textFont = Poppins({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+})
 
 const MarketingPage = () => {
   return (
     <div className="flex items-center justify-center flex-col">
-      <div className="flex items-center justify-center flex-col">
+      <div
+        className={cn(
+          'flex items-center justify-center flex-col',
+          headingFont.className
+        )}
+      >
         <div className="mb-4 flex items-center border shadow-sm p-4 bg-amber-100 text-amber-700 rounded-full uppercase">
           <Medal className="h-6 w-6 mr-2" />
           No 1 task management
@@ -18,7 +35,12 @@ const MarketingPage = () => {
           work forward.
         </div>
       </div>
-      <div className="text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl text-center mx-auto">
+      <div
+        className={cn(
+          'text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl text-center mx-auto',
+          textFont.className
+        )}
+      >
         Collaborate, manage projects, and reach new productivity peaks. From
         high rises to the home office, the way your team work is unique -
         accomplish it all with Taskify.

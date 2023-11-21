c260dfec36a48f35c1e4cee50878631737cd541d
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-21 14:04:09 +1300

    add footer

---
+++ app/(marketing)/_components/Footer.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import { Logo } from '@/components/Logo'
+import { Button } from '@/components/ui/button'
+
+const Footer = () => {
+  return (
+    <div className="fixed bottom-0 w-full px-4 border-t bg-slate-100">
+      <div className="md:max-w-screen-2xl mx-auto flex items-center w-full justify-between">
+        <Logo />
+        <div className="space-x-4 md:block md:w-auto flex items-center  justify-between w-full">
+          <Button size="sm" variant="ghost">
+            Privacy Policy
+          </Button>
+          <Button size="sm" variant="ghost">
+            Terms of Service
+          </Button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Footer

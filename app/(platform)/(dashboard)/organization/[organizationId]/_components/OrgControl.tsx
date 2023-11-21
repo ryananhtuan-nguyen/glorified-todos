b3430e607c9e57068976e96ce5ac6791437c7618
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-21 18:39:03 +1300

    new comp

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/OrgControl.tsx
@@ -0,0 +1,11 @@
+'use client'
+import React from 'react'
+import { useOrganizationList } from '@clerk/nextjs'
+import { useParams } from 'next/navigation'
+
+const OrgControl = () => {
+  const params = useParams()
+  return null
+}
+
+export default OrgControl

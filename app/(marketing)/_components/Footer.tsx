a7138fbc8897635913dd17e37db9fa6367c6c365
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-21 14:30:00 +1300

    footer done

--- app/(marketing)/_components/Footer.tsx
+++ app/(marketing)/_components/Footer.tsx
@@ -5,7 +5,7 @@ import { Button } from '@/components/ui/button'
 
 const Footer = () => {
   return (
-    <div className="fixed bottom-0 w-full px-4 border-t bg-slate-100">
+    <div className="fixed bottom-0 w-full p-4 border-t bg-slate-100">
       <div className="md:max-w-screen-2xl mx-auto flex items-center w-full justify-between">
         <Logo />
         <div className="space-x-4 md:block md:w-auto flex items-center  justify-between w-full">

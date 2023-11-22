c349e338872ce0b143c8bebf3ab17748cd0f6c5d
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-23 12:54:53 +1300

    const routes

--- app/(platform)/(dashboard)/_components/NavItem.tsx
+++ app/(platform)/(dashboard)/_components/NavItem.tsx
@@ -1,5 +1,10 @@
 'use client'
+import Image from 'next/image'
 import React from 'react'
+import { Activity, CreditCard, Layout, Settings } from 'lucide-react'
+
+import { cn } from '@/lib/utils'
+import { AccordionItem, AccordionTrigger } from '@/components/ui/accordion'
 
 export type Organization = {
   id: string
@@ -21,7 +26,49 @@ const NavItem = ({
   onExpand,
   organization,
 }: NavItemProps) => {
-  return <div>NavItem</div>
+  const routes = [
+    {
+      label: 'Boards',
+      icon: <Layout className="h-4 w-4 mr-2" />,
+      href: `/organization/${organization.id}`,
+    },
+    {
+      label: 'Activity',
+      icon: <Activity className="h-4 w-4 mr-2" />,
+      href: `/organization/${organization.id}/activity`,
+    },
+    {
+      label: 'Settings',
+      icon: <Settings className="h-4 w-4 mr-2" />,
+      href: `/organization/${organization.id}/settings`,
+    },
+  ]
+
+  return (
+    <AccordionItem value={organization.id} className="border-none">
+      <AccordionTrigger
+        onClick={() => onExpand(organization.id)}
+        className={cn(
+          'flex items-center gap-x-2 p-1.5 text-neutral-700 rounded-md hover:bg-neutral-500/10 transition text-start no-underline hover:no-underline',
+          {
+            'bg-sky-500/10 text-sky-70': isActive && !isExpanded,
+          }
+        )}
+      >
+        <div className="flex items-center gap-x-2">
+          <div className="w-7 h-7 relative">
+            <Image
+              fill
+              src={organization.imageUrl}
+              alt="organization"
+              className="rounded-sm object-cover"
+            />
+          </div>
+          <span className="font-medium text-sm">{organization.name}</span>
+        </div>
+      </AccordionTrigger>
+    </AccordionItem>
+  )
 }
 
 export default NavItem

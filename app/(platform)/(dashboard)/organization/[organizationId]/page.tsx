dbb3fb1f2609a4b2074ee07c9c6581364107fb50
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-23 15:17:26 +1300

    server action function

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,9 +1,30 @@
-import { OrganizationSwitcher, auth } from '@clerk/nextjs'
-import React from 'react'
+import { db } from '@/lib/db'
 
 const OrganizationIdPage = () => {
-  const { userId, orgId } = auth()
-  return <div>Organization Page</div>
+  async function create(formData: FormData) {
+    'use server'
+    const title = formData.get('title') as string
+
+    await db.board.create({
+      data: {
+        title,
+      },
+    })
+  }
+
+  return (
+    <div>
+      <form action={create}>
+        <input
+          id="title"
+          name="title"
+          required
+          placeholder="Enter a board title"
+          className="border-black border p-1"
+        />
+      </form>
+    </div>
+  )
 }
 
 export default OrganizationIdPage

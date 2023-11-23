dfb9d9a40bfcbfa2499cee4762f57a3857e67560
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-23 17:32:47 +1300

    debugging

--- actions/create-board.ts
+++ actions/create-board.ts
@@ -1,22 +1,45 @@
 'use server'
 import { db } from '@/lib/db'
 import { revalidatePath } from 'next/cache'
+import { redirect } from 'next/navigation'
 import { z } from 'zod'
 
+export type State = {
+  errors?: {
+    title?: string[]
+  }
+  message?: string | null
+}
+
 const CreateBoard = z.object({
-  title: z.string(),
+  title: z
+    .string()
+    .min(3, { message: 'Minimum length of 3 letters is required' }),
 })
 
-export async function create(formData: FormData) {
-  const { title } = CreateBoard.parse({
+export async function create(prevState: State, formData: FormData) {
+  const validatedFields = CreateBoard.safeParse({
     title: formData.get('title'),
   })
 
-  await db.board.create({
-    data: {
-      title,
-    },
-  })
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing fields.',
+    }
+  }
 
+  const { title } = validatedFields.data
+
+  try {
+    await db.board.create({
+      data: {
+        title,
+      },
+    })
+  } catch (error) {
+    return { message: 'Database Error' }
+  }
   revalidatePath('/organization/org_2YTLXOCYWhdSyPcWySSsUGR8Ev2')
+  redirect('/organization/org_2YTLXOCYWhdSyPcWySSsUGR8Ev2')
 }

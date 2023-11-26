e2e4672f07757fd5300d1de062ab3d34763e5cdd
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-27 12:47:28 +1300

    customized input

--- components/form/FormInput.tsx
+++ components/form/FormInput.tsx
@@ -3,6 +3,10 @@
 import React, { forwardRef } from 'react'
 import { useFormStatus } from 'react-dom'
 
+import { Label } from '../ui/label'
+import { Input } from '../ui/input'
+import { cn } from '@/lib/utils'
+
 interface FormInputProps {
   id: string
   label?: string
@@ -34,8 +38,30 @@ const FormInput = forwardRef<HTMLInputElement, FormInputProps>(
   ) => {
     const { pending } = useFormStatus()
     return (
-      <div className="space-y-2 ">
-        <div className="space-y-1">{label ? <div>Label</div> : null}</div>
+      <div className="space-y-2">
+        <div className="space-y-1">
+          {label ? (
+            <Label
+              htmlFor={id}
+              className="text-xs font-semibold text-neutral-700"
+            >
+              Label
+            </Label>
+          ) : null}
+          <Input
+            onBlur={onBlur}
+            defaultValue={defaultValue}
+            ref={ref}
+            required={required}
+            name={id}
+            id={id}
+            placeholder={placeholder}
+            type={type}
+            disabled={pending || disabled}
+            className={cn('text-sm px-2 py-1 h-7', className)}
+            aria-describedby={`${id}-error`}
+          />
+        </div>
       </div>
     )
   }

cdaab1917fa06465360605dbffb97d4c2f9b134a
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-27 14:40:42 +1300

    displaying board

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/BoardList.tsx
@@ -0,0 +1,24 @@
+import { User2 } from 'lucide-react'
+import React from 'react'
+
+const BoardList = () => {
+  return (
+    <div className="space-y-4">
+      <div className="flex items-center font-semibold text-lg text-neutral-700">
+        <User2 className="h-6 w-6 mr-2" />
+        Your boards
+      </div>
+      <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
+        <div
+          role="button"
+          className="aspect-video relative h-full w-full bg-muted rounded-sm flex flex-col gap-y-1 items-center justify-center hover:opacity-75 transition"
+        >
+          <p className="text-sm">Create new board</p>
+          <span className="text-xs">5 remaining</span>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default BoardList

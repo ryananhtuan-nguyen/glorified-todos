50854920099b11bef6aa622dd98f478f2011d9d1
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-30 12:39:45 +1300

    adding more data

--- actions/create-board/index.ts
+++ actions/create-board/index.ts
@@ -8,22 +8,41 @@ import { createSafeAction } from '@/lib/create-safe-action'
 import { CreateBoard } from './schema'
 
 const handler = async (data: InputType): Promise<ReturnType> => {
-  const { userId } = auth()
+  const { userId, orgId } = auth()
 
-  if (!userId) {
+  if (!userId || !orgId) {
     return {
       error: 'Unauthorized',
     }
   }
 
-  const { title } = data
+  const { title, image } = data
 
+  const [imageId, imageThumbUrl, imageFullUrl, imageLinkHTML, imageUserName] =
+    image.split('|')
+  if (
+    !imageId ||
+    !imageThumbUrl ||
+    !imageFullUrl ||
+    !imageLinkHTML ||
+    !imageUserName
+  ) {
+    return {
+      error: 'Missing fields. Failed to create board.',
+    }
+  }
   let board
 
   try {
     board = await db.board.create({
       data: {
         title,
+        orgId,
+        imageId,
+        imageFullUrl,
+        imageThumbUrl,
+        imageLinkHTML,
+        imageUserName,
       },
     })
   } catch (error) {

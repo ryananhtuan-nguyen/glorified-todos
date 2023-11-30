89fac0b87418d62742b617e63240ec37beff266b
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-30 14:09:42 +1300

    changed title for document

--- app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -1,6 +1,15 @@
 import React from 'react'
+import { startCase } from 'lodash'
+import { auth } from '@clerk/nextjs'
 
 import OrgControl from './_components/OrgControl'
+
+export async function generateMetadata() {
+  const { orgSlug } = auth()
+  return {
+    title: startCase(orgSlug || 'organization'),
+  }
+}
 
 const OrganizationIdLayout = ({ children }: { children: React.ReactNode }) => {
   return (

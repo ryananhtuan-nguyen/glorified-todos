de9c55d4aac1b66a6ab51fba7191da60c235c655
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-30 17:17:02 +1300

    init Board Options

---
+++ app/(platform)/(dashboard)/board/[boardId]/_components/BoardOptions.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import React from 'react'
+
+interface BoardOptionsProps {
+  id: string
+}
+
+const BoardOptions = ({ id }: BoardOptionsProps) => {
+  return <div>BoardOptions</div>
+}
+
+export default BoardOptions

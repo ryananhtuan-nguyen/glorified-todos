5e4ee7061112b1e5543605ef848b92f164711e90
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-30 15:53:34 +1300

    added boardtitle

---
+++ app/(platform)/(dashboard)/board/[boardId]/_components/BoardNavbar.tsx
@@ -0,0 +1,16 @@
+import { Board } from '@prisma/client'
+import BoardTitleForm from './BoardTitleForm'
+
+interface BoardNavbarProps {
+  data: Board
+}
+
+const BoardNavbar = async ({ data }: BoardNavbarProps) => {
+  return (
+    <div className="w-full h-14 z-[40] bg-black/50 fixed top-14 flex items-center px-6 gap-x-4 text-white">
+      <BoardTitleForm data={data} />
+    </div>
+  )
+}
+
+export default BoardNavbar

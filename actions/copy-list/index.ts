883b2427a10970b64a58884df0eba95aa9a0e1f8
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-12-01 17:25:26 +1300

    copylist function

--- actions/copy-list/index.ts
+++ actions/copy-list/index.ts
@@ -5,7 +5,7 @@ import { auth } from '@clerk/nextjs'
 import { createSafeAction } from '@/lib/create-safe-action'
 import { db } from '@/lib/db'
 import { revalidatePath } from 'next/cache'
-import { DeleteList } from './schema'
+import { CopyList } from './schema'
 import { InputType, ReturnType } from './types'
 
 const handler = async (data: InputType): Promise<ReturnType> => {
@@ -19,7 +19,7 @@ const handler = async (data: InputType): Promise<ReturnType> => {
   let list
 
   try {
-    list = await db.list.delete({
+    const listToCopy = await db.list.findUnique({
       where: {
         id,
         boardId,
@@ -27,13 +27,48 @@ const handler = async (data: InputType): Promise<ReturnType> => {
           orgId,
         },
       },
+      include: {
+        cards: true,
+      },
+    })
+
+    if (!listToCopy) {
+      return { error: 'List not found' }
+    }
+
+    const lastList = await db.list.findFirst({
+      where: { boardId },
+      orderBy: { order: 'desc' },
+      select: { order: true },
+    })
+
+    const newOrder = lastList ? lastList.order + 1 : 1
+
+    list = await db.list.create({
+      data: {
+        boardId: listToCopy.boardId,
+        title: `${listToCopy.title} - Copy`,
+        order: newOrder,
+        cards: {
+          createMany: {
+            data: listToCopy.cards.map((card) => ({
+              title: card.title,
+              description: card.description,
+              order: card.order,
+            })),
+          },
+        },
+      },
+      include: {
+        cards: true,
+      },
     })
   } catch (error) {
-    return { error: 'Failed to delete.' }
+    return { error: 'Failed to copy.' }
   }
 
   revalidatePath(`/board/${boardId}`)
   return { data: list }
 }
 
-export const deleteList = createSafeAction(DeleteList, handler)
+export const copyList = createSafeAction(CopyList, handler)

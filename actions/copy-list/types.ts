a70b35cebc7f8a6613cdff57bbd95dae1fcc4e21
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-12-01 17:25:47 +1300

    Type for copy list

--- actions/copy-list/types.ts
+++ actions/copy-list/types.ts
@@ -2,8 +2,8 @@ import { z } from 'zod'
 import { List } from '@prisma/client'
 
 import { ActionState } from '@/lib/create-safe-action'
-import { DeleteList } from './schema'
+import { CopyList } from './schema'
 
-export type InputType = z.infer<typeof DeleteList>
+export type InputType = z.infer<typeof CopyList>
 
 export type ReturnType = ActionState<InputType, List>

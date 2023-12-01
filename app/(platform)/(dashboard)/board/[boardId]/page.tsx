69f9e70da250f55fe2853be74f08f03c1848bff2
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-12-01 13:38:34 +1300

    passed props to container

--- app/(platform)/(dashboard)/board/[boardId]/page.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/page.tsx
@@ -1,7 +1,43 @@
+import { db } from '@/lib/db'
+import { auth } from '@clerk/nextjs'
+import { redirect } from 'next/navigation'
 import React from 'react'
+import ListContainer from './_components/ListContainer'
 
-const BoardIdPage = () => {
-  return <div>BoardIdPage</div>
+interface BoardIdPageProps {
+  params: {
+    boardId: string
+  }
+}
+
+const BoardIdPage = async ({ params: { boardId } }: BoardIdPageProps) => {
+  const { orgId } = auth()
+  if (!orgId) redirect(`/select-org`)
+
+  const lists = await db.list.findMany({
+    where: {
+      boardId,
+      board: {
+        orgId,
+      },
+    },
+    include: {
+      cards: {
+        orderBy: {
+          order: 'asc',
+        },
+      },
+    },
+    orderBy: {
+      order: 'asc',
+    },
+  })
+
+  return (
+    <div className="p-4 h-full overflow-x-auto">
+      <ListContainer boardId={boardId} data={lists} />
+    </div>
+  )
 }
 
 export default BoardIdPage

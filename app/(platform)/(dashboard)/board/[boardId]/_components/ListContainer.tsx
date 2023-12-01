1eaaec43605d408853ad1ce6202a169e1f1b61f8
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-12-01 16:08:01 +1300

    added lists

--- app/(platform)/(dashboard)/board/[boardId]/_components/ListContainer.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/ListContainer.tsx
@@ -1,7 +1,10 @@
 'use client'
 
+import { useEffect, useState } from 'react'
+
 import { ListWithCards } from '@/types'
 import ListForm from './ListForm'
+import ListItem from './ListItem'
 
 interface ListContainerProps {
   data: ListWithCards[]
@@ -9,8 +12,17 @@ interface ListContainerProps {
 }
 
 const ListContainer = ({ data, boardId }: ListContainerProps) => {
+  const [orderedData, setOrderedData] = useState(data)
+
+  useEffect(() => {
+    setOrderedData(data)
+  }, [data])
+
   return (
-    <ol>
+    <ol className="flex gap-x-3 h-full">
+      {orderedData.map((list, index) => {
+        return <ListItem key={list.id} index={index} data={list} />
+      })}
       <ListForm />
       <div className="flex-shrink-0 w-1" />
     </ol>

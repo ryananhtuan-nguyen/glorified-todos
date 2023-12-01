87808d90b1782163d306b45daa1acb8b67cd6181
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-12-01 17:12:52 +1300

    init

---
+++ actions/copy-list/index.ts
@@ -0,0 +1,39 @@
+'use server'
+
+import { auth } from '@clerk/nextjs'
+
+import { createSafeAction } from '@/lib/create-safe-action'
+import { db } from '@/lib/db'
+import { revalidatePath } from 'next/cache'
+import { DeleteList } from './schema'
+import { InputType, ReturnType } from './types'
+
+const handler = async (data: InputType): Promise<ReturnType> => {
+  const { userId, orgId } = auth()
+  if (!userId || !orgId) {
+    return { error: 'Unauthorized' }
+  }
+
+  const { id, boardId } = data
+
+  let list
+
+  try {
+    list = await db.list.delete({
+      where: {
+        id,
+        boardId,
+        board: {
+          orgId,
+        },
+      },
+    })
+  } catch (error) {
+    return { error: 'Failed to delete.' }
+  }
+
+  revalidatePath(`/board/${boardId}`)
+  return { data: list }
+}
+
+export const deleteList = createSafeAction(DeleteList, handler)

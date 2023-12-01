11e59ed7c39d6536e626f9565495e0137c366221
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-12-01 14:21:50 +1300

    fixed type

--- actions/create-list/types.ts
+++ actions/create-list/types.ts
@@ -1,9 +1,9 @@
 import { z } from 'zod'
-import { Board } from '@prisma/client'
+import { List } from '@prisma/client'
 
 import { ActionState } from '@/lib/create-safe-action'
 import { CreateList } from './schema'
 
 export type InputType = z.infer<typeof CreateList>
 
-export type ReturnType = ActionState<InputType, Board>
+export type ReturnType = ActionState<InputType, List>

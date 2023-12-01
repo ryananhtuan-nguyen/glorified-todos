8019ff2344a839532b377019e995233c029c7793
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-12-01 16:24:56 +1300

    list title update functions

--- app/(platform)/(dashboard)/board/[boardId]/_components/ListHeader.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/ListHeader.tsx
@@ -1,7 +1,10 @@
 'use client'
+import { updateList } from '@/actions/update-list'
 import FormInput from '@/components/form/FormInput'
+import { useAction } from '@/hooks/use-action'
 import { List } from '@prisma/client'
 import React, { ElementRef, useEffect, useRef, useState } from 'react'
+import { toast } from 'sonner'
 import { useEventListener } from 'usehooks-ts'
 
 interface ListHeaderProps {
@@ -26,6 +29,31 @@ const ListHeader = ({ data }: ListHeaderProps) => {
     setIsEditing(false)
   }
 
+  const { execute } = useAction(updateList, {
+    onSuccess: (data) => {
+      toast.success(`Renamed to "${data.title}"`)
+      setTitle(data.title)
+      disableEditing()
+    },
+    onError: (error) => {
+      toast.error(error)
+    },
+  })
+
+  const handleSubmit = (formData: FormData) => {
+    const title = formData.get('title') as string
+    const id = formData.get('id') as string
+    const boardId = formData.get('boardId') as string
+    if (title === data.title) {
+      return disableEditing()
+    }
+    execute({ title, id, boardId })
+  }
+
+  const onBlur = () => {
+    formRef.current?.requestSubmit()
+  }
+
   const onKeyDown = (e: KeyboardEvent) => {
     if (e.key === 'Escape') {
       formRef.current?.requestSubmit()
@@ -37,17 +65,18 @@ const ListHeader = ({ data }: ListHeaderProps) => {
   return (
     <div className="pt-2 px-2 text-sm font-semibold flex justify-between items-start gap-x-2">
       {isEditing ? (
-        <form className="flex-1 px-[2px]">
+        <form className="flex-1 px-[2px]" action={handleSubmit} ref={formRef}>
           <input hidden id="id" name="id" value={data.id} />
           <input hidden id="boardId" name="boardId" value={data.boardId} />
           <FormInput
             ref={inputRef}
-            onBlur={() => {}}
+            onBlur={onBlur}
             id="title"
             placeholder="Enter list title ..."
             defaultValue={title}
             className="text-sm px-[7px] py-1 h-7 font-medium border-transparent hover:border-input focus:border-input transition truncate bg-transparent focus:bg-white"
           />
+          <button type="submit" hidden />
         </form>
       ) : (
         <div

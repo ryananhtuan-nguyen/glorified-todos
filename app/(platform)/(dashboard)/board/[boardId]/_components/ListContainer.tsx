521f0131b00f1efd22357b3fd40ffd9a2af235ff
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-12-01 13:38:41 +1300

    imported custom type

---
+++ app/(platform)/(dashboard)/board/[boardId]/_components/ListContainer.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import { ListWithCards } from '@/types'
+
+interface ListContainerProps {
+  data: ListWithCards[]
+  boardId: string
+}
+
+const ListContainer = ({ data, boardId }: ListContainerProps) => {
+  return <div>ListContainer</div>
+}
+
+export default ListContainer

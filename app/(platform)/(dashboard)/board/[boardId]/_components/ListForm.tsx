a6b28735233549acad2b814ccda70191d145354a
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-12-01 14:09:19 +1300

    UI button add list

--- app/(platform)/(dashboard)/board/[boardId]/_components/ListForm.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/ListForm.tsx
@@ -1,13 +1,74 @@
 'use client'
 
-import React from 'react'
+import React, { ElementRef, useRef, useState } from 'react'
+import { Plus, X } from 'lucide-react'
+
 import ListWrapper from './ListWrapper'
-import { Plus } from 'lucide-react'
+import { useEventListener, useOnClickOutside } from 'usehooks-ts'
+import FormInput from '@/components/form/FormInput'
+import { useParams } from 'next/navigation'
+import FormSubmit from '@/components/form/FormSubmit'
+import { Button } from '@/components/ui/button'
 
 const ListForm = () => {
+  const params = useParams()
+
+  const formRef = useRef<ElementRef<'form'>>(null)
+  const inputRef = useRef<ElementRef<'input'>>(null)
+
+  const [isEditing, setIsEditing] = useState<boolean>(false)
+
+  const enableEditing = () => {
+    setIsEditing(true)
+    setTimeout(() => {
+      inputRef.current?.focus()
+    })
+  }
+
+  const disableEditing = () => {
+    setIsEditing(false)
+  }
+
+  const onKeyDown = (e: KeyboardEvent) => {
+    if (e.key === 'Escape') {
+      disableEditing()
+    }
+  }
+
+  useEventListener('keydown', onKeyDown)
+  useOnClickOutside(formRef, disableEditing)
+
+  if (isEditing) {
+    return (
+      <ListWrapper>
+        <form
+          ref={formRef}
+          className="w-full p-3 rounded-md bg-white space-y-4 shadow-md"
+        >
+          <FormInput
+            ref={inputRef}
+            className="text-sm px-2 py-1 h-7 font-medium border-transparent hover:border-input focus:border-input transition"
+            id="title"
+            placeholder="Enter list title..."
+          />
+          <input hidden value={params.boardId} name="boardId" />
+          <div className="flex items-center gap-x-1">
+            <FormSubmit>Add list</FormSubmit>
+            <Button onClick={disableEditing} size="sm" variant="ghost">
+              <X className="h-5 w-5" />
+            </Button>
+          </div>
+        </form>
+      </ListWrapper>
+    )
+  }
+
   return (
     <ListWrapper>
-      <button className="w-full rounded-md bg-white/80 hover:bg-white/50 transition p-3 flex items-center font-medium text-sm">
+      <button
+        onClick={enableEditing}
+        className="w-full rounded-md bg-white/80 hover:bg-white/50 transition p-3 flex items-center font-medium text-sm"
+      >
         <Plus className="h-4 w-4 mr-2" />
         Add a list
       </button>

86f1accab039fc316500aea00bdb471c534a74d0
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-12-04 17:49:14 +1300

    8:19:15

--- app/(platform)/(dashboard)/board/[boardId]/_components/CardForm.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/CardForm.tsx
@@ -1,6 +1,8 @@
 'use client'
+import FormSubmit from '@/components/form/FormSubmit'
+import FormTextarea from '@/components/form/FormTextarea'
 import { Button } from '@/components/ui/button'
-import { Plus } from 'lucide-react'
+import { Plus, X } from 'lucide-react'
 import React, { forwardRef } from 'react'
 
 interface CardFormProps {
@@ -12,6 +14,26 @@ interface CardFormProps {
 
 const CardForm = forwardRef<HTMLTextAreaElement, CardFormProps>(
   ({ listId, enableEditing, disableEditing, isEditing }, ref) => {
+    if (isEditing) {
+      return (
+        <form className="m-1 py-0.5 px-1 space-y-4">
+          <FormTextarea
+            id="title"
+            onKeyDown={() => {}}
+            ref={ref}
+            placeholder="Enter a title for this card..."
+          />
+          <input hidden id="listId" name="listId" value={listId} />
+          <div className="flex items-center gap-x-1">
+            <FormSubmit>Add card</FormSubmit>
+            <Button onClick={disableEditing} size="sm" variant="ghost">
+              <X className="h-5 w-5" />
+            </Button>
+          </div>
+        </form>
+      )
+    }
+
     return (
       <div className="pt-2 px-2">
         <Button

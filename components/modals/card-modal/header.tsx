a9d9fbc42310236ca4f858f666516d9e225265b6
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2023-12-07 21:35:18 +1300

    feat: fullstack card title change

--- components/modals/card-modal/header.tsx
+++ components/modals/card-modal/header.tsx
@@ -1,10 +1,15 @@
 'use client'
-import { useState } from 'react'
+import { ElementRef, useRef, useState } from 'react'
 
 import FormInput from '@/components/form/FormInput'
 import { CardWithList } from '@/types'
 import { Layout } from 'lucide-react'
 import { Skeleton } from '@/components/ui/skeleton'
+import { useQueryClient } from '@tanstack/react-query'
+import { useParams } from 'next/navigation'
+import { useAction } from '@/hooks/use-action'
+import { updateCard } from '@/actions/update-card'
+import { toast } from 'sonner'
 
 interface HeaderProps {
   data: CardWithList
@@ -13,17 +18,60 @@ interface HeaderProps {
 const Header = ({ data }: HeaderProps) => {
   const [title, setTitle] = useState(data.title)
 
+  const queryClient = useQueryClient()
+  const { execute } = useAction(updateCard, {
+    onSuccess: (data) => {
+      queryClient.invalidateQueries({
+        queryKey: ['card', data.id],
+      })
+
+      toast.success(`Renamed to "${data.title}"`)
+      setTitle(data.title)
+    },
+    onError: (error) => {
+      toast.error(error)
+    },
+  })
+
+  const params = useParams()
+
+  const inputRef = useRef<ElementRef<'input'>>(null)
+
+  const onBlur = () => {
+    inputRef.current?.form?.requestSubmit()
+  }
+
+  const onSubmit = (formData: FormData) => {
+    const title = formData.get('title') as string
+    const boardId = params.boardId as string
+
+    if (title === data.title) {
+      return
+    }
+
+    execute({
+      title,
+      boardId,
+      id: data.id,
+    })
+  }
+
   return (
     <div className="flex items-start gap-x-3 mb-6 w-full">
       <Layout className="h-5 w-5 mt-1 text-neutral-700" />
       <div className="w-full">
-        <form>
+        <form action={onSubmit}>
           <FormInput
+            ref={inputRef}
+            onBlur={onBlur}
             id="title"
             defaultValue={title}
             className="font-semibold text-xl px-1 text-neutral-700 bg-transparent border-transparent relative -left-1.5 w-[95%] focus-visible:bg-white focus-visible:border-input mb-0.5 truncate"
           />
         </form>
+        <p className="text-sm text-muted-foreground">
+          in list <span className="underline">{data.list.title}</span>
+        </p>
       </div>
     </div>
   )

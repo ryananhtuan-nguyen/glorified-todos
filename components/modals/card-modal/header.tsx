bb6702392534e1d3126e4a9deb99eb266a12dfd8
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2023-12-07 21:09:32 +1300

    style: header skeleton

--- components/modals/card-modal/header.tsx
+++ components/modals/card-modal/header.tsx
@@ -4,13 +4,14 @@ import { useState } from 'react'
 import FormInput from '@/components/form/FormInput'
 import { CardWithList } from '@/types'
 import { Layout } from 'lucide-react'
+import { Skeleton } from '@/components/ui/skeleton'
 
 interface HeaderProps {
-  data?: CardWithList
+  data: CardWithList
 }
 
 const Header = ({ data }: HeaderProps) => {
-  const [title, setTitle] = useState(data?.title)
+  const [title, setTitle] = useState(data.title)
 
   return (
     <div className="flex items-start gap-x-3 mb-6 w-full">
@@ -29,3 +30,15 @@ const Header = ({ data }: HeaderProps) => {
 }
 
 export default Header
+
+Header.Skeleton = function HeaderSkeleton() {
+  return (
+    <div className="flex items-start gap-x-3 mb-6">
+      <Skeleton className="h-6 w-6 mt-1 bg-neutral-200" />
+      <div>
+        <Skeleton className="w-24 h-6 mb-1 bg-neutral-200" />
+        <Skeleton className="w-12 h-4 bg-neutral-200" />
+      </div>
+    </div>
+  )
+}

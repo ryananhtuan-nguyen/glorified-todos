ab19c235ffd9856b8e28390ac9e19323a30b49e4
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2023-12-07 13:32:11 +1300

    feat: displaying cards

--- app/(platform)/(dashboard)/board/[boardId]/_components/ListItem.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/ListItem.tsx
@@ -4,6 +4,8 @@ import { ListWithCards } from '@/types'
 import ListHeader from './ListHeader'
 import { ElementRef, useRef, useState } from 'react'
 import CardForm from './CardForm'
+import { cn } from '@/lib/utils'
+import CardItem from './CardItem'
 
 interface ListItemProps {
   index: number
@@ -30,6 +32,17 @@ const ListItem = ({ index, data }: ListItemProps) => {
     <li className="shrink-0 h-full w-[272px] select-none">
       <div className="w-full rounded-md bg-[#f1f2f4] shadow-md pb-2">
         <ListHeader onAddCard={enableEditing} data={data} />
+        <ol
+          className={cn(
+            'mx-1 px-1 py-0.5 flex flex-col gap-y-2',
+            data.cards.length > 0 ? 'mt-2' : 'mt-0'
+          )}
+        >
+          {data.cards.map((card, index) => (
+            <CardItem index={index} key={card.id} data={card} />
+          ))}
+        </ol>
+
         <CardForm
           ref={textareaRef}
           isEditing={isEditing}

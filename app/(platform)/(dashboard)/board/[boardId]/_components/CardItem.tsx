b50894a5109ffc45a1a648e0d9d90cf4eed5fc04
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2023-12-07 13:31:48 +1300

    chore: remove consolelog

---
+++ app/(platform)/(dashboard)/board/[boardId]/_components/CardItem.tsx
@@ -0,0 +1,19 @@
+'use client'
+
+import { Card } from '@prisma/client'
+import React from 'react'
+
+interface CardItemProps {
+  data: Card
+  index: number
+}
+
+const CardItem = ({ data, index }: CardItemProps) => {
+  return (
+    <div className="truncate border-2 border-transparent hover:border-black py-2 px-3 text-sm bg-white rounded-md shadow-sm">
+      {data.title}
+    </div>
+  )
+}
+
+export default CardItem

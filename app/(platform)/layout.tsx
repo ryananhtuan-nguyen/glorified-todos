7d2c32f053b1ced408d06315c1b5dc0662143ac4
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2023-12-07 15:39:07 +1300

    add provider

--- app/(platform)/layout.tsx
+++ app/(platform)/layout.tsx
@@ -1,11 +1,14 @@
-import { ClerkProvider } from '@clerk/nextjs'
 import React from 'react'
 import { Toaster } from 'sonner'
+import { ClerkProvider } from '@clerk/nextjs'
+
+import { ModalProvider } from '@/components/providers/modal-provider'
 
 const PlatformLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <ClerkProvider>
       <Toaster richColors />
+      <ModalProvider />
       {children}
     </ClerkProvider>
   )

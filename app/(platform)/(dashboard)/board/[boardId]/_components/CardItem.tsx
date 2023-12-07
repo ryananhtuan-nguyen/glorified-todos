ea0cf17ffb0b398abef1ba26a66ecda471631d12
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2023-12-07 13:38:59 +1300

    chore: button role

--- app/(platform)/(dashboard)/board/[boardId]/_components/CardItem.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/CardItem.tsx
@@ -10,7 +10,10 @@ interface CardItemProps {
 
 const CardItem = ({ data, index }: CardItemProps) => {
   return (
-    <div className="truncate border-2 border-transparent hover:border-black py-2 px-3 text-sm bg-white rounded-md shadow-sm">
+    <div
+      role="button"
+      className="truncate border-2 border-transparent hover:border-black py-2 px-3 text-sm bg-white rounded-md shadow-sm"
+    >
       {data.title}
     </div>
   )

bd2690049a43a1a390f4bf132017d5fd5f243dea
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2023-12-07 13:56:27 +1300

    feat: drag and drop

--- app/(platform)/(dashboard)/board/[boardId]/_components/ListContainer.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/ListContainer.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useEffect, useState } from 'react'
+import { DragDropContext, Droppable } from '@hello-pangea/dnd'
 
 import { ListWithCards } from '@/types'
 import ListForm from './ListForm'
@@ -19,13 +20,24 @@ const ListContainer = ({ data, boardId }: ListContainerProps) => {
   }, [data])
 
   return (
-    <ol className="flex gap-x-3 h-full">
-      {orderedData.map((list, index) => {
-        return <ListItem key={list.id} index={index} data={list} />
-      })}
-      <ListForm />
-      <div className="flex-shrink-0 w-1" />
-    </ol>
+    <DragDropContext onDragEnd={() => {}}>
+      <Droppable droppableId="lists" type="list" direction="horizontal">
+        {(provided) => (
+          <ol
+            {...provided.droppableProps}
+            ref={provided.innerRef}
+            className="flex gap-x-3 h-full"
+          >
+            {orderedData.map((list, index) => {
+              return <ListItem key={list.id} index={index} data={list} />
+            })}
+            {provided.placeholder}
+            <ListForm />
+            <div className="flex-shrink-0 w-1" />
+          </ol>
+        )}
+      </Droppable>
+    </DragDropContext>
   )
 }
 

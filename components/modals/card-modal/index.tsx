359e4610227ad960d08023b192c33507994ded1e
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2023-12-07 22:19:14 +1300

    feat: Description component with editing

--- components/modals/card-modal/index.tsx
+++ components/modals/card-modal/index.tsx
@@ -7,6 +7,7 @@ import { useCardModal } from '@/hooks/use-card-modal'
 import { CardWithList } from '@/types'
 import { fetcher } from '@/lib/fetcher'
 import Header from './header'
+import Description from './description'
 
 const CardModal = () => {
   const id = useCardModal((state) => state.id)
@@ -22,6 +23,17 @@ const CardModal = () => {
     <Dialog open={isOpen} onOpenChange={onClose}>
       <DialogContent>
         {!cardData ? <Header.Skeleton /> : <Header data={cardData} />}
+        <div className="grid grid-cols-1 md:grid-cols-4 md:gap-4">
+          <div className="col-span-3">
+            <div className="w-full space-y-6">
+              {!cardData ? (
+                <Description.Skeleton />
+              ) : (
+                <Description data={cardData} />
+              )}
+            </div>
+          </div>
+        </div>
       </DialogContent>
     </Dialog>
   )

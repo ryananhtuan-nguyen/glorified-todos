89b2cb08d055acfa58ab4165a5d0432fb750327c
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2023-12-07 15:53:45 +1300

    refactior: wrap with query client

--- app/(platform)/layout.tsx
+++ app/(platform)/layout.tsx
@@ -3,13 +3,16 @@ import { Toaster } from 'sonner'
 import { ClerkProvider } from '@clerk/nextjs'
 
 import { ModalProvider } from '@/components/providers/modal-provider'
+import { QueryProvider } from '@/components/providers/query-provider'
 
 const PlatformLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <ClerkProvider>
-      <Toaster richColors />
-      <ModalProvider />
-      {children}
+      <QueryProvider>
+        <Toaster richColors />
+        <ModalProvider />
+        {children}
+      </QueryProvider>
     </ClerkProvider>
   )
 }

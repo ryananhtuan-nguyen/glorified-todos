f480f865271a3bdfedfbe18af51242f103c93860
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2023-12-08 17:10:14 +1300

    feat:logs for update board

--- actions/update-board/index.ts
+++ actions/update-board/index.ts
@@ -7,6 +7,8 @@ import { db } from '@/lib/db'
 import { revalidatePath } from 'next/cache'
 import { createSafeAction } from '@/lib/create-safe-action'
 import { UpdateBoard } from './schema'
+import { createAuditLog } from '@/lib/create-audit-log'
+import { ACTION, ENTITY_TYPE } from '@prisma/client'
 
 const handler = async (data: InputType): Promise<ReturnType> => {
   const { userId, orgId } = auth()
@@ -28,6 +30,15 @@ const handler = async (data: InputType): Promise<ReturnType> => {
         title,
       },
     })
+
+    await createAuditLog({
+      entityTitle: board.title,
+      entityId: board.id,
+      entityType: ENTITY_TYPE.BOARD,
+      action: ACTION.UPDATE,
+    })
+
+    //
   } catch (error) {
     return { error: 'Failed to update.' }
   }

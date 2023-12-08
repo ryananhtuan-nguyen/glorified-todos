140c16f6b9f5c760cec1da0bad208f1e0e899d3c
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2023-12-08 17:04:33 +1300

    feat: logs for create list

--- actions/create-list/index.ts
+++ actions/create-list/index.ts
@@ -7,6 +7,8 @@ import { db } from '@/lib/db'
 import { revalidatePath } from 'next/cache'
 import { createSafeAction } from '@/lib/create-safe-action'
 import { CreateList } from './schema'
+import { createAuditLog } from '@/lib/create-audit-log'
+import { ACTION, ENTITY_TYPE } from '@prisma/client'
 
 const handler = async (data: InputType): Promise<ReturnType> => {
   const { userId, orgId } = auth()
@@ -48,6 +50,13 @@ const handler = async (data: InputType): Promise<ReturnType> => {
         boardId,
         order: newOrder,
       },
+    })
+
+    await createAuditLog({
+      entityTitle: list.title,
+      entityId: list.id,
+      entityType: ENTITY_TYPE.LIST,
+      action: ACTION.CREATE,
     })
 
     //

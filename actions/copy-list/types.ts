3abdb02b80e52bee03111fb92a54ea9f6528216e
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2023-12-08 13:09:32 +1300

    feat: types for copyCard

--- actions/copy-list/types.ts
+++ actions/copy-list/types.ts
@@ -1,9 +1,9 @@
 import { z } from 'zod'
-import { List } from '@prisma/client'
+import { Card } from '@prisma/client'
 
 import { ActionState } from '@/lib/create-safe-action'
-import { CopyList } from './schema'
+import { CopyCard } from './schema'
 
-export type InputType = z.infer<typeof CopyList>
+export type InputType = z.infer<typeof CopyCard>
 
-export type ReturnType = ActionState<InputType, List>
+export type ReturnType = ActionState<InputType, Card>

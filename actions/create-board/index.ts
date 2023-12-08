240068d0149d1d52ba725fdc88b8b5b093a3df2e
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2023-12-08 17:02:40 +1300

    feat: log for create board

--- actions/create-board/index.ts
+++ actions/create-board/index.ts
@@ -6,6 +6,8 @@ import { db } from '@/lib/db'
 import { revalidatePath } from 'next/cache'
 import { createSafeAction } from '@/lib/create-safe-action'
 import { CreateBoard } from './schema'
+import { createAuditLog } from '@/lib/create-audit-log'
+import { ACTION, ENTITY_TYPE } from '@prisma/client'
 
 const handler = async (data: InputType): Promise<ReturnType> => {
   const { userId, orgId } = auth()
@@ -46,6 +48,15 @@ const handler = async (data: InputType): Promise<ReturnType> => {
         imageUserName,
       },
     })
+
+    await createAuditLog({
+      entityTitle: board.title,
+      entityId: board.id,
+      entityType: ENTITY_TYPE.CARD,
+      action: ACTION.CREATE,
+    })
+
+    //
   } catch (error) {
     return {
       error: 'Failed to create.',

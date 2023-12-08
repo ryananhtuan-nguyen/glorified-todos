31d67e0435324c5261e087a58ed0ddf66c5224a9
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2023-12-08 16:53:47 +1300

    feat: showing activities

--- components/modals/card-modal/activity.tsx
+++ components/modals/card-modal/activity.tsx
@@ -1,7 +1,9 @@
 'use client'
 
+import ActivityItem from '@/components/ActivityItem'
 import { Skeleton } from '@/components/ui/skeleton'
 import { AuditLog } from '@prisma/client'
+import { ActivityIcon } from 'lucide-react'
 import React from 'react'
 
 interface ActivityProps {
@@ -9,7 +11,19 @@ interface ActivityProps {
 }
 
 const Activity = ({ items }: ActivityProps) => {
-  return <div>Activity</div>
+  return (
+    <div className="flex items-start gap-x-3 w-full">
+      <ActivityIcon className="h-5 w-5 mt-0.5 text-neutral-700" />
+      <div className="w-full ">
+        <p className="font-semibold text-neutral-700 mb-2">Activity</p>
+        <ol className="mt-2 space-y-4">
+          {items.map((item) => (
+            <ActivityItem key={item.id} data={item} />
+          ))}
+        </ol>
+      </div>
+    </div>
+  )
 }
 
 export default Activity

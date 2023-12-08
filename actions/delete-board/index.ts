a2237ab58b5d570cad39a5f18f75d740bc7fb603
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2023-12-08 17:06:44 +1300

    feat: logs for delete board

--- actions/delete-board/index.ts
+++ actions/delete-board/index.ts
@@ -8,6 +8,8 @@ import { revalidatePath } from 'next/cache'
 import { createSafeAction } from '@/lib/create-safe-action'
 import { DeleteBoard } from './schema'
 import { redirect } from 'next/navigation'
+import { createAuditLog } from '@/lib/create-audit-log'
+import { ACTION, ENTITY_TYPE } from '@prisma/client'
 
 const handler = async (data: InputType): Promise<ReturnType> => {
   const { userId, orgId } = auth()
@@ -25,6 +27,13 @@ const handler = async (data: InputType): Promise<ReturnType> => {
         id,
         orgId,
       },
+    })
+
+    await createAuditLog({
+      entityTitle: board.title,
+      entityId: board.id,
+      entityType: ENTITY_TYPE.BOARD,
+      action: ACTION.DELETE,
     })
   } catch (error) {
     return { error: 'Failed to delete.' }

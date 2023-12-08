8bc67044be95dacdf4ca4a1b69a04b84acf49ea1
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2023-12-08 14:11:07 +1300

    feat: log for create card

--- actions/create-card/index.ts
+++ actions/create-card/index.ts
@@ -7,6 +7,8 @@ import { db } from '@/lib/db'
 import { revalidatePath } from 'next/cache'
 import { createSafeAction } from '@/lib/create-safe-action'
 import { CreateCard } from './schema'
+import { createAuditLog } from '@/lib/create-audit-log'
+import { ACTION, ENTITY_TYPE } from '@prisma/client'
 
 const handler = async (data: InputType): Promise<ReturnType> => {
   const { userId, orgId } = auth()
@@ -39,6 +41,7 @@ const handler = async (data: InputType): Promise<ReturnType> => {
     })
 
     const newOrder = lastCard ? lastCard.order + 1 : 1
+
     card = await db.card.create({
       data: {
         title,
@@ -47,7 +50,14 @@ const handler = async (data: InputType): Promise<ReturnType> => {
       },
     })
 
-    //
+    await createAuditLog({
+      entityId: card.id,
+      entityTitle: card.title,
+      entityType: ENTITY_TYPE.CARD,
+      action: ACTION.CREATE,
+    })
+
+    //n
   } catch (error) {
     return { error: 'Failed to create.' }
   }

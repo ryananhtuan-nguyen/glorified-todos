a915c9053596ab6c993a7920b6d8a8fd289672f3
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2023-12-08 17:11:08 +1300

    feat: logs for update card

--- actions/update-card/index.ts
+++ actions/update-card/index.ts
@@ -7,6 +7,8 @@ import { db } from '@/lib/db'
 import { revalidatePath } from 'next/cache'
 import { createSafeAction } from '@/lib/create-safe-action'
 import { UpdateCard } from './schema'
+import { createAuditLog } from '@/lib/create-audit-log'
+import { ACTION, ENTITY_TYPE } from '@prisma/client'
 
 const handler = async (data: InputType): Promise<ReturnType> => {
   const { userId, orgId } = auth()
@@ -32,6 +34,15 @@ const handler = async (data: InputType): Promise<ReturnType> => {
         ...values,
       },
     })
+
+    await createAuditLog({
+      entityTitle: card.title,
+      entityId: card.id,
+      entityType: ENTITY_TYPE.CARD,
+      action: ACTION.UPDATE,
+    })
+
+    //
   } catch (error) {
     return { error: 'Failed to update.' }
   }

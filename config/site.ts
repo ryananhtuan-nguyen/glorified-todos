a7d1be93923c463dd678adddcd5bf93da875ff05
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2023-12-09 19:12:38 +1300

    chore: change description

--- config/site.ts
+++ config/site.ts
@@ -1,4 +1,5 @@
 export const siteConfig = {
   name: 'Glorified Todos',
-  description: 'Collaborate, manage projects, and reach new productivity peaks',
+  description:
+    "Elevate your team's productivity with Glorified Todos—a dynamic platform designed to propel your projects forward! Whether you're conquering tasks in a high-rise office or a cozy home workspace, our collaborative tool is tailored to the unique rhythm of your team, ensuring you reach new peaks of efficiency. Unleash the power of teamwork, project management, and seamless collaboration—all in one place!",
 }

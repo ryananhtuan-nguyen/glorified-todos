89f366754c5e77ae51e4408cbc64635ed92313c7
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2023-12-09 16:02:23 +1300

    refactor: dynamic plan name

--- app/(platform)/(dashboard)/organization/[organizationId]/_components/Info.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/Info.tsx
@@ -5,7 +5,11 @@ import { useOrganization } from '@clerk/nextjs'
 import { CreditCard } from 'lucide-react'
 import Image from 'next/image'
 
-const Info = () => {
+interface InfoProps {
+  isPro: boolean
+}
+
+const Info = ({ isPro }: InfoProps) => {
   const { organization, isLoaded } = useOrganization()
   if (!isLoaded) return <Info.Skeleton />
   return (
@@ -22,7 +26,7 @@ const Info = () => {
         <p className="font-semibold text-xl">{organization?.name}</p>
         <div className="flex items-center text-xs text-muted-foreground">
           <CreditCard className="h-3 w-3 mr-1" />
-          Free
+          {isPro ? 'Pro' : 'Free'}
         </div>
       </div>
     </div>

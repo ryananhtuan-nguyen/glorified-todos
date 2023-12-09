772f2d882ed8a244a015a78b5a2bc630701159a8
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2023-12-09 16:18:00 +1300

    chore: config name

--- components/modals/ProModal.tsx
+++ components/modals/ProModal.tsx
@@ -31,10 +31,10 @@ const ProModal = () => {
         </div>
         <div className="text-neutral-700 mx-auto space-y-6 p-6">
           <h2 className="font-semibold text-xl">
-            Upgrade to Taskify Pro Today!
+            Upgrade to GlorifiedTodos Pro Today!
           </h2>
           <p className="text-xs font-semibold text-neutral-600">
-            Explore the best of Taskify
+            Explore the best of GlorifiedTodos
           </p>
           <div className="pl-3">
             <ul className="text-sm list-disc">

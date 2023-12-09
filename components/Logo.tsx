1037fe3024aea0f9a63a6bc9ea2fccb09b548bbc
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2023-12-09 16:17:58 +1300

    chore: config name

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -16,7 +16,7 @@ export const Logo = () => {
         <p
           className={cn('text-lg text-neutral-700 pb-1', headingFont.className)}
         >
-          Taskify
+          GlorifiedTodos
         </p>
       </div>
     </Link>

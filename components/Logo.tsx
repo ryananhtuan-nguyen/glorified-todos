c04898b4708f69ebcaa9862efacf1f12751e7685
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2023-12-09 16:33:13 +1300

    style: changed logo

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -12,7 +12,7 @@ export const Logo = () => {
   return (
     <Link href="/">
       <div className="hover:opacity-75 transition items-center gap-x-2 hidden md:flex">
-        <Image src="/logo.svg" alt="logo" height={30} width={30} />
+        <Image src="/book.jpg" alt="logo" height={30} width={30} />
         <p
           className={cn('text-lg text-neutral-700 pb-1', headingFont.className)}
         >

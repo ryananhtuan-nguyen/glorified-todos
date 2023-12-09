7370609dbd057d8c75ef8d4878383fa28b0f65b2
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2023-12-09 19:27:48 +1300

    style: edit text

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -5,6 +5,7 @@ import { Poppins } from 'next/font/google'
 
 import { Button } from '@/components/ui/button'
 import { cn } from '@/lib/utils'
+import { format } from 'date-fns'
 
 const headingFont = localFont({
   src: '../../public/fonts/font.woff2',
@@ -24,29 +25,30 @@ const MarketingPage = () => {
           headingFont.className
         )}
       >
-        <div className="mb-4 flex items-center border shadow-sm p-4 bg-amber-100 text-amber-700 rounded-full uppercase">
+        <div className="mb-4 flex items-center border shadow-sm p-4 bg-red-200 text-blue-700 rounded-full uppercase">
           <Medal className="h-6 w-6 mr-2" />
-          No 1 task management
+          Best Task-Management of {format(new Date(), 'yyyy')}
         </div>
         <h1 className="text-3xl md:text-6xl text-center  text-neutral-800 mb-6">
-          Glorified Todos helps team move
+          Glorified Todos helps teams elevate
         </h1>
-        <div className="text-3xl md:text-6xl bg-gradient-to-r from-fuchsia-600 to-pink-600 text-white px-4 p-2 rounded-md pb-4 w-fit">
-          work forward.
+        <div className="text-3xl md:text-6xl bg-gradient-to-r from-blue-300 to-pink-400 text-white px-4 p-2 rounded-md pb-4 w-fit">
+          Project Momentum
         </div>
       </div>
       <div
         className={cn(
-          'text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl text-center mx-auto',
+          'text-sm md:text-xl text-neutral-400 mt-12 mb-12 max-w-xs md:max-w-2xl text-left mx-auto',
           textFont.className
         )}
       >
-        Collaborate, manage projects, and reach new productivity peaks. From
-        high rises to the home office, the way your team work is unique -
-        accomplish it all with Glorified Todos.
+        Foster collaboration, project management mastery, and attain
+        unprecedented productivity heights. Whether your team operates from
+        towering corporate offices or cozy home workspaces, embrace the
+        uniqueness of your workflow—achieve it all with Glorified Todos.
       </div>
-      <Button className="mt-6" size="lg" asChild>
-        <Link href="/sign-up"> Get Glorified Todos for free</Link>
+      <Button className="mt-6 bg-orange-400" size="lg" asChild>
+        <Link href="/sign-up"> Get GlorifiedTodos for free</Link>
       </Button>
     </div>
   )

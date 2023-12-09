0ffaf120d3ca777f225b34aaf5e7440bf5962089
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2023-12-09 15:11:56 +1300

    feat: add modal to pro plan

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useState } from 'react'
 import CardModal from '../modals/card-modal'
+import ProModal from '../modals/ProModal'
 
 export const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false)
@@ -17,6 +18,7 @@ export const ModalProvider = () => {
   return (
     <>
       <CardModal />
+      <ProModal />
     </>
   )
 }
